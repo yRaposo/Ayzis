0279c9510c4d47898afbdfd6349e2a3906479ae4
yRaposo <ynyght@gmail.com>
2025-02-11 17:02:13 -0300

    Adiciona estrutura inicial para o componente Dashboard e implementa função de recuperação de vendas

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,6 +1,12 @@
 'use client'
 
-export default function dashboard() {
+import { useCallback, useState } from "react";
+
+export default function Dashboard() {
+
+    const getVendas = useCallback(async () => {
+
+    })
 
     return (
         <div className="flex flex-col gap-8 items-center justify-center min-h-screen">
